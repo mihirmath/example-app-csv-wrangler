63aed19a714989318c3ad7e1f44ae20c25bedbfe
mihirmath <mihirmathur88@gmail.com>
2023-07-20 18:23:24 -0700

    basic framework

--- app.py
+++ app.py
@@ -1,137 +1,33 @@
 import streamlit as st
-import pandas as pd
 
-###################################
-from st_aggrid import AgGrid
-from st_aggrid.grid_options_builder import GridOptionsBuilder
-from st_aggrid.shared import JsCode
+# Define your menu items as a list of tuples (menu_item_name, ingredient_1, ingredient_2, ...)
+menu_items = [
+    ("Pizza", "Dough", "Tomato Sauce", "Cheese", "Toppings"),
+    ("Burger", "Bun", "Patty", "Lettuce", "Cheese", "Tomato", "Onion"),
+    ("Pasta", "Pasta", "Tomato Sauce", "Meatballs", "Cheese"),
+]
 
-###################################
+def calculate_ingredients(menu_item, num_people):
+    st.write(f"Menu Item: {menu_item}")
+    st.write(f"Number of People: {num_people}")
 
-from functionforDownloadButtons import download_button
+    # Assuming each menu item requires one unit of each ingredient per person
+    for ingredient in menu_item[1:]:
+        ingredient_quantity = num_people
+        st.write(f"{ingredient}: {ingredient_quantity} units")
 
-###################################
+def main():
+    st.title("Menu Ingredients Calculator")
 
+    # Dropdown for selecting the menu item
+    selected_menu_item = st.selectbox("Select Menu Item", menu_items)
 
-def _max_width_():
-    max_width_str = f"max-width: 1800px;"
-    st.markdown(
-        f"""
-    <style>
-    .reportview-container .main .block-container{{
-        {max_width_str}
-    }}
-    </style>    
-    """,
-        unsafe_allow_html=True,
-    )
+    # Number input for the number of people
+    num_people = st.number_input("Enter the Number of People", min_value=1, value=1)
 
-st.set_page_config(page_icon="✂️", page_title="CSV Wrangler")
+    # Calculate and display the ingredients
+    if st.button("Calculate"):
+        calculate_ingredients(selected_menu_item, num_people)
 
-# st.image("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/balloon_1f388.png", width=100)
-st.image(
-    "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/scissors_2702-fe0f.png",
-    width=100,
-)
-
-st.title("CSV Wrangler")
-
-# st.caption(
-#     "PRD : TBC | Streamlit Ag-Grid from Pablo Fonseca: https://pypi.org/project/streamlit-aggrid/"
-# )
-
-
-# ModelType = st.radio(
-#     "Choose your model",
-#     ["Flair", "DistilBERT (Default)"],
-#     help="At present, you can choose between 2 models (Flair or DistilBERT) to embed your text. More to come!",
-# )
-
-# with st.expander("ToDo's", expanded=False):
-#     st.markdown(
-#         """
-# -   Add pandas.json_normalize() - https://streamlit.slack.com/archives/D02CQ5Z5GHG/p1633102204005500
-# -   **Remove 200 MB limit and test with larger CSVs**. Currently, the content is embedded in base64 format, so we may end up with a large HTML file for the browser to render
-# -   **Add an encoding selector** (to cater for a wider array of encoding types)
-# -   **Expand accepted file types** (currently only .csv can be imported. Could expand to .xlsx, .txt & more)
-# -   Add the ability to convert to pivot → filter → export wrangled output (Pablo is due to change AgGrid to allow export of pivoted/grouped data)
-# 	    """
-#     )
-# 
-#     st.text("")
-
-
-c29, c30, c31 = st.columns([1, 6, 1])
-
-with c30:
-
-    uploaded_file = st.file_uploader(
-        "",
-        key="1",
-        help="To activate 'wide mode', go to the hamburger menu > Settings > turn on 'wide mode'",
-    )
-
-    if uploaded_file is not None:
-        file_container = st.expander("Check your uploaded .csv")
-        shows = pd.read_csv(uploaded_file)
-        uploaded_file.seek(0)
-        file_container.write(shows)
-
-    else:
-        st.info(
-            f"""
-                👆 Upload a .csv file first. Sample to try: [biostats.csv](https://people.sc.fsu.edu/~jburkardt/data/csv/biostats.csv)
-                """
-        )
-
-        st.stop()
-
-from st_aggrid import GridUpdateMode, DataReturnMode
-
-gb = GridOptionsBuilder.from_dataframe(shows)
-# enables pivoting on all columns, however i'd need to change ag grid to allow export of pivoted/grouped data, however it select/filters groups
-gb.configure_default_column(enablePivot=True, enableValue=True, enableRowGroup=True)
-gb.configure_selection(selection_mode="multiple", use_checkbox=True)
-gb.configure_side_bar()  # side_bar is clearly a typo :) should by sidebar
-gridOptions = gb.build()
-
-st.success(
-    f"""
-        💡 Tip! Hold the shift key when selecting rows to select multiple rows at once!
-        """
-)
-
-response = AgGrid(
-    shows,
-    gridOptions=gridOptions,
-    enable_enterprise_modules=True,
-    update_mode=GridUpdateMode.MODEL_CHANGED,
-    data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
-    fit_columns_on_grid_load=False,
-)
-
-df = pd.DataFrame(response["selected_rows"])
-
-st.subheader("Filtered data will appear below 👇 ")
-st.text("")
-
-st.table(df)
-
-st.text("")
-
-c29, c30, c31 = st.columns([1, 1, 2])
-
-with c29:
-
-    CSVButton = download_button(
-        df,
-        "File.csv",
-        "Download to CSV",
-    )
-
-with c30:
-    CSVButton = download_button(
-        df,
-        "File.csv",
-        "Download to TXT",
-    )
+if __name__ == "__main__":
+    main()
